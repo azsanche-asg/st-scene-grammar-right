5e287123531f677771111376b572dd35399d96bd
azsanche-asg <asg415347@gmail.com>
2025-11-02 19:36:26 +0100

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,7 @@
-from st_scene_grammar.eval.runner import run_experiment
-from st_scene_grammar.utils.config import load_config
+from eval.runner import run_experiment
+from utils.config import load_config
 def test_synthetic_runs():
-    cfg = load_config('st_scene_grammar/configs/synthetic_demo.yaml')
+    cfg = load_config('configs/synthetic_demo.yaml')
     res = run_experiment(cfg, cfg['save']['out_dir'])
     assert 'tag' in res and res['cr'] >= 0.0
     assert 0.0 <= res['purity'] <= 1.0
